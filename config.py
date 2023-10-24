2bbf5cefc51d2001fa1d90d82292e8129119ca8e
rpurei <rpurey@gmail.com>
2023-10-24 20:40:12 +0300

    Добавлен config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+LOG_FOLDER = 'log'
+LOG_FILE = 'app.log'
+LOG_MAX_BYTES = 1048576
+LOG_COUNT = 9
+LOG_FORMAT = '%(asctime)s [%(levelname)s] %(name)s : (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
+
+IPA_SERVER = '[адрес IPA сервера]'
+IPA_USER = '[логин IPA сервера]'
+IPA_PASSWD = '[пароль IPA сервера]'
